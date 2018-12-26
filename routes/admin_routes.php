e6fd0c62c014f978129f5107163df25000bac81f
Milos <grantlatest@gmail.com>
2018-12-26 13:47:04 +0100

    route

--- routes/admin_routes.php
+++ routes/admin_routes.php
@@ -72,4 +72,24 @@ Route::group(['as' => $as, 'middleware' => ['auth', 'permission:ADMIN_PANEL']], 
 	Route::get(config('laraadmin.adminRoute') . '/backup_dt_ajax', 'LA\BackupsController@dtajax');
 	Route::post(config('laraadmin.adminRoute') . '/create_backup_ajax', 'LA\BackupsController@create_backup_ajax');
 	Route::get(config('laraadmin.adminRoute') . '/downloadBackup/{id}', 'LA\BackupsController@downloadBackup');
+
+	/* ================== Contacts ================== */
+	Route::resource(config('laraadmin.adminRoute') . '/contacts', 'LA\ContactsController');
+	Route::get(config('laraadmin.adminRoute') . '/contact_dt_ajax', 'LA\ContactsController@dtajax');
+
+	/* ================== Leads ================== */
+	Route::resource(config('laraadmin.adminRoute') . '/leads', 'LA\LeadsController');
+	Route::get(config('laraadmin.adminRoute') . '/lead_dt_ajax', 'LA\LeadsController@dtajax');
+
+	/* ================== Opportunies ================== */
+	Route::resource(config('laraadmin.adminRoute') . '/opportunities', 'LA\OpportunitiesController');
+	Route::get(config('laraadmin.adminRoute') . '/opportunity_dt_ajax', 'LA\OpportunitiesController@dtajax');
+
+	/* ================== Tickets ================== */
+	Route::resource(config('laraadmin.adminRoute') . '/tickets', 'LA\TicketsController');
+	Route::get(config('laraadmin.adminRoute') . '/ticket_dt_ajax', 'LA\TicketsController@dtajax');
+
+	/* ================== Projects ================== */
+	Route::resource(config('laraadmin.adminRoute') . '/projects', 'LA\ProjectsController');
+	Route::get(config('laraadmin.adminRoute') . '/project_dt_ajax', 'LA\ProjectsController@dtajax');
 });
